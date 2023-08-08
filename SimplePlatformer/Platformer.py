149b1adc056a5623cf05f9f19ef04554d6070589
Ejtys <jwziebapn@gmail.com>
2023-08-08 15:03:27 +0200

    Add scene

--- SimplePlatformer/Platformer.py
+++ SimplePlatformer/Platformer.py
@@ -8,45 +8,45 @@ class MyGame(arcade.Window):
         
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
         
-        #Init vars for sprites lists
-        self.wall_list = None
-        self.player_list = None
-        
+        #Init scene var
+        self.scene = None
         #Init var for player sprite
         self.player = None
         
 
     def setup(self):
+        #Init scene
+        self.scene = arcade.Scene()
+        
         #Init sprites lists
-        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
-        self.player_list = arcade.SpriteList()
+        self.scene.add_sprite_list("Player")
+        self.scene.add_sprite_list("Walls", use_spatial_hash=True)
         
         #Init player
         self.player = arcade.Sprite(cons.PLAYER_IMAGE, cons.CHARACTER_SCALING)
         self.player.center_x = 64
         self.player.center_y = 128
-        self.player_list.append(self.player)
+        self.scene.add_sprite("Playe", self.player)
         
         #Init walls
         for x in range(0, 1250, 64):
             wall = arcade.Sprite(cons.WALL_IMAGE, cons.TILE_SCALING)
             wall.center_x = x
             wall.center_y = 32
-            self.wall_list.append(wall)
+            self.scene.add_sprite("Walls", wall)
             
         coordinate_list = [[512, 96], [256, 96], [768, 96]]
 
         for coordinate in coordinate_list:
             box = arcade.Sprite(cons.BOX_IMAGE, cons.TILE_SCALING)
             box.position = coordinate
-            self.wall_list.append(box)
+            self.scene.add_sprite("Walls", box)
     
     def on_draw(self):
         
         self.clear()
         
-        self.player_list.draw()
-        self.wall_list.draw()
+        self.scene.draw()
 
 
 
