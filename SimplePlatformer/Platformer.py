22b524956d1ff15d46019f8afc47c2a35a42682f
Ejtys <jwziebapn@gmail.com>
2023-08-08 22:31:29 +0200

    Add two levels

--- SimplePlatformer/Platformer.py
+++ SimplePlatformer/Platformer.py
@@ -20,11 +20,15 @@ class MyGame(arcade.Window):
         #Init sounds
         self.collect_coin_sound = arcade.load_sound(cons.COLLECT_COIN_SOUND)
         self.jump_sound = arcade.load_sound(cons.JUMP_SOUND)
+        self.game_over_sound = arcade.load_sound(cons.GAME_OVER_SOUND)
         
         #Track pressed keys
         self.pressed_keys = set()
         
         self.score = 0
+        self.reset_score = True
+        self.end_of_map = 0
+        self.level = 1
         
 
     def setup(self):
@@ -33,19 +37,23 @@ class MyGame(arcade.Window):
         self.gui_camera = arcade.Camera(cons.SCREEN_WIDTH, cons.SCREEN_HEIGHT)
         
         #Init scene
-        self.tile_map = arcade.load_tilemap(cons.TILE_MAP, cons.TILE_SCALING, cons.LAYER_OPTIONS)
+        self.tile_map = arcade.load_tilemap(cons.LEVEL_MAP[self.level - 1], 
+                                            cons.TILE_SCALING, 
+                                            cons.LAYER_OPTIONS)
         
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
         
         if self.tile_map.background_color:
             arcade.set_background_color(self.tile_map.background_color)
         
+        self.scene.add_sprite_list_after("Player", cons.LAYER_NAME_FOREGROUND)
+        
         #Init player
-        self.scene.add_sprite_list("Player")
         self.player = arcade.Sprite(cons.PLAYER_IMAGE, cons.CHARACTER_SCALING)
-        self.player.center_x = 64
-        self.player.center_y = 128
+        self.player.center_x = cons.PLAYER_START_X
+        self.player.center_y = cons.PLAYER_START_Y
         self.scene.add_sprite("Playe", self.player)
+        
         
         #Init engine
         
@@ -53,8 +61,11 @@ class MyGame(arcade.Window):
                                                      gravity_constant=cons.GRAVITY, 
                                                      walls = self.scene["Platforms"])
         
+        if self.reset_score:
+            self.score = 0
+        self.reset_score = True
         
-        self.score = 0
+        self.end_of_map = self.tile_map.width * cons.GRID_PIXEL_SIZE
     
     def update_player_movement(self):
         up = {arcade.key.UP, arcade.key.W}
@@ -102,6 +113,27 @@ class MyGame(arcade.Window):
             coin.remove_from_sprite_lists()
             arcade.play_sound(self.collect_coin_sound)
             self.score += 1
+            
+        if self.player.center_y < -100:
+            self.player.center_x = cons.PLAYER_START_X
+            self.player.center_y = cons.PLAYER_START_Y
+
+            arcade.play_sound(self.game_over_sound)
+            
+        if arcade.check_for_collision_with_list(
+            self.player, self.scene[cons.LAYER_NAME_DONT_TOUCH]
+        ):
+            self.player.change_x = 0
+            self.player.change_y = 0
+            self.player.center_x = cons.PLAYER_START_X
+            self.player.center_y = cons.PLAYER_START_Y
+
+            arcade.play_sound(self.game_over_sound)
+            
+        if self.player.center_x >= self.end_of_map:
+            self.level += 1
+            self.reset_score = False
+            self.setup()
     
     def on_draw(self):       
         self.clear()
--- SimplePlatformer/cons.py
+++ SimplePlatformer/cons.py
@@ -6,9 +6,15 @@ CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
 COIN_SCALING = 0.5
 
+SPRITE_PIXEL_SIZE = 128
+GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
+
 PLAYER_MOVEMENT_SPEED = 5
-GRAVITY = 1.3
+GRAVITY = 1
 PLAYER_JUMP_SPEED = 20
+
+PLAYER_START_X = 64
+PLAYER_START_Y = 225
 
 PLAYER_IMAGE = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
 WALL_IMAGE = ":resources:images/tiles/grassMid.png"
@@ -16,11 +22,25 @@ BOX_IMAGE = ":resources:images/tiles/boxCrate.png"
 
 COLLECT_COIN_SOUND = ":resources:sounds/coin1.wav"
 JUMP_SOUND = ":resources:sounds/jump3.wav"
+GAME_OVER_SOUND = ":resources:sounds/gameover1.wav"
 
-TILE_MAP = "SimplePlatformer/map/map.json"
+LEVEL_MAP = ["SimplePlatformer/map/map2_level_1.json", "SimplePlatformer/map/map2_level_2.json"]
+
+LAYER_NAME_PLATFORMS = "Platforms"
+LAYER_NAME_COINS = "Coins"
+LAYER_NAME_FOREGROUND = "Foreground"
+LAYER_NAME_BACKGROUND = "Background"
+LAYER_NAME_DONT_TOUCH = "Don't Touch"
 
 LAYER_OPTIONS = {
-            "Platforms": {
+            LAYER_NAME_PLATFORMS: {
+                "use_spatial_hash": True,
+            },
+            LAYER_NAME_COINS: {
+                "use_spatial_hash": True,
+            },
+            LAYER_NAME_DONT_TOUCH: {
                 "use_spatial_hash": True,
             },
         }
+
