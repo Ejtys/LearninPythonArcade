f3f628c6cdd739d28877f32114d43a36ad2e7074
Ejtys <jwziebapn@gmail.com>
2023-08-08 14:48:28 +0200

    Add player sprite

--- SimplePlatformer/Platformer.py
+++ SimplePlatformer/Platformer.py
@@ -7,13 +7,31 @@ class MyGame(arcade.Window):
         super().__init__(cons.SCREEN_WIDTH, cons.SCREEN_HEIGHT, cons.SCREEN_TITLE)
         
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+        
+        #Init vars for sprites lists
+        self.wall_list = None
+        self.player_list = None
+        
+        #Init var for player sprite
+        self.player = None
+        
 
     def setup(self):
-        pass
+        #Init sprites lists
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        self.player_list = arcade.SpriteList()
+        
+        #Init player
+        self.player = arcade.Sprite(cons.PLAYER_IMAGE, cons.CHARACTER_SCALING)
+        self.player.center_x = 64
+        self.player.center_y = 128
+        self.player_list.append(self.player)
     
     def on_draw(self):
         
         self.clear()
+        
+        self.player_list.draw()
 
 
 
--- SimplePlatformer/cons.py
+++ SimplePlatformer/cons.py
@@ -1,3 +1,9 @@
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Platformer"
+
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+
+
+PLAYER_IMAGE = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
